3ced4b0f87a58ef77eace4b296942430896b5554
Alessandro Bessi <alessandro.bessi@mail.com>
2018-10-27 05:54:42 +0200

    Implements Sarsa.

---
+++ sarsa/sarsa.py
@@ -0,0 +1,66 @@
+import itertools
+import numpy as np
+from collections import defaultdict, namedtuple
+from typing import Dict, Callable, Tuple
+from tqdm import tqdm
+from envs.windygridworld import WindyGridworldEnv
+from viz import viz
+
+EpisodeStats = namedtuple('Stats', ['length', 'reward'])
+
+
+def make_epsilon_greedy_policy(Q: Dict,
+                               epsilon: float,
+                               num_actions: int) -> Callable[[int], np.array]:
+    def policy_fn(state: int):
+        A = np.ones(num_actions, dtype=float) * epsilon / num_actions
+        best_action = np.argmax(Q[state])
+        A[best_action] += (1.0 - epsilon)
+        return A
+
+    return policy_fn
+
+
+def sarsa(env: WindyGridworldEnv,
+          num_episodes: float,
+          discount: float = 1.0,
+          alpha: float = 0.5,
+          epsilon: float = 0.1) -> Tuple[Dict, EpisodeStats]:
+    Q = defaultdict(lambda: np.zeros(env.action_space.n))
+
+    num_episodes = int(num_episodes)
+    stats = EpisodeStats(np.zeros(num_episodes), np.zeros(num_episodes))
+    policy = make_epsilon_greedy_policy(Q, epsilon, env.action_space.n)
+
+    for i in tqdm(range(num_episodes)):
+
+        state = env.reset()
+        action_probs = policy(state)
+        action = np.random.choice(np.arange(len(action_probs)), p=action_probs)
+
+        for t in itertools.count():
+            next_state, reward, done, _ = env.step(action)
+            next_action_probs = policy(next_state)
+            next_action = np.random.choice(np.arange(len(next_action_probs)), p=next_action_probs)
+
+            stats.reward[i] += reward
+            stats.length[i] = t
+
+            td_target = reward + discount * Q[next_state][next_action]
+            td_delta = td_target - Q[state][action]
+            Q[state][action] += alpha * td_delta
+
+            if done:
+                break
+
+            action = next_action
+            state = next_state
+
+    return Q, stats
+
+
+if __name__ == '__main__':
+    env = WindyGridworldEnv()
+
+    Q, stats = sarsa(env, 1e4)
+    viz.plot_episode_stats(stats)
--- viz/viz.py
+++ viz/viz.py
@@ -1,7 +1,11 @@
 from typing import Dict, List, Tuple, Union
+from collections import namedtuple
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
+
+EpisodeStats = namedtuple('Stats', ['length', 'reward'])
 
 
 def plot_surface(x: np.array, y: np.array, z: np.array, title: str) -> None:
@@ -37,3 +41,41 @@ def plot_value_function(v: Dict[Tuple, float],
 
     plot_surface(x, y, z_no_ace, '{} (No Usable Ace)'.format(title))
     plot_surface(x, y, z_ace, '{} (Usable Ace)'.format(title))
+
+
+def plot_episode_stats(stats: EpisodeStats,
+                       smoothing_window: int = 10,
+                       noshow: bool = False):
+    fig1 = plt.figure(figsize=(10, 5))
+    plt.plot(stats.length)
+    plt.xlabel('Episode')
+    plt.ylabel('Episode Length')
+    plt.title('Episode Length over Time')
+    if noshow:
+        plt.close(fig1)
+    else:
+        plt.show(fig1)
+
+    fig2 = plt.figure(figsize=(10, 5))
+    rewards_smoothed = pd.Series(stats.reward). \
+        rolling(smoothing_window, min_periods=smoothing_window).mean()
+    plt.plot(rewards_smoothed)
+    plt.xlabel('Episode')
+    plt.ylabel('Episode Reward (Smoothed)')
+    plt.title('Episode Reward over Time (Smoothed over window size {})'.format(smoothing_window))
+    if noshow:
+        plt.close(fig2)
+    else:
+        plt.show(fig2)
+
+    fig3 = plt.figure(figsize=(10, 5))
+    plt.plot(np.cumsum(stats.length), np.arange(len(stats.length)))
+    plt.xlabel('Time Steps')
+    plt.ylabel('Episode')
+    plt.title('Episode per time step')
+    if noshow:
+        plt.close(fig3)
+    else:
+        plt.show(fig3)
+
+    return fig1, fig2, fig3
