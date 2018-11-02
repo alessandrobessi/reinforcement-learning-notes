e853f0198d6d27aaf46e47d38dd1060cd35c3132
Alessandro Bessi <alessandro.bessi@mail.com>
2018-11-02 06:37:08 +0100

    Implements function approximation.

---
+++ function-approximation/functionapproximation.py
@@ -0,0 +1,190 @@
+import gym
+import itertools
+import numpy as np
+from typing import Union, Callable
+from collections import namedtuple
+from viz import viz
+from tqdm import tqdm
+from sklearn.linear_model import SGDRegressor
+from sklearn.kernel_approximation import RBFSampler
+from sklearn.pipeline import FeatureUnion
+from sklearn.preprocessing import StandardScaler
+
+EpisodeStats = namedtuple('Stats', ['length', 'reward'])
+
+
+class Estimator:
+
+    @staticmethod
+    def featurize_state(state):
+        """
+        Returns the featurized representation for a state.
+        """
+        scaled = scaler.transform([state])
+        featurized = featurizer.transform(scaled)
+        return featurized[0]
+
+    def __init__(self):
+        # We create a separate model for each action in the environment's
+        # action space. Alternatively we could somehow encode the action
+        # into the features, but this way it's easier to code up.
+        self.models = []
+        for _ in range(env.action_space.n):
+            model = SGDRegressor(learning_rate='constant', tol=1e-3)
+            # We need to call partial_fit once to initialize the model
+            # or we get a NotFittedError when trying to make a prediction
+            # This is quite hacky.
+            model.partial_fit([self.featurize_state(env.reset())], [0])
+            self.models.append(model)
+
+    def predict(self, s: int, a: int = None) -> Union[int, np.array]:
+        """
+        Makes value function predictions.
+
+        Args:
+            s: state to make a prediction for
+            a: (Optional) action to make a prediction for
+
+        Returns
+            If an action a is given this returns a single number as the prediction.
+            If no action is given this returns a vector or predictions for all actions
+            in the environment where pred[i] is the prediction for action i.
+
+        """
+        features = self.featurize_state(s)
+        if not a:
+            return np.array([m.predict([features])[0] for m in self.models])
+        return self.models[a].predict([features])[0]
+
+    def update(self, s: int, a: int, y: int) -> None:
+        """
+        Updates the estimator parameters for a given state and action towards
+        the target y.
+        """
+        features = self.featurize_state(s)
+        self.models[a].partial_fit([features], [y])
+
+
+def make_epsilon_greedy_policy(estimator: Estimator,
+                               epsilon: float,
+                               num_actions: int) -> Callable[[int], np.array]:
+    def policy_fn(state: int) -> np.array:
+        A = np.ones(num_actions, dtype=float) * epsilon / num_actions
+        q_values = estimator.predict(state)
+        best_action = np.argmax(q_values)
+        A[best_action] += (1.0 - epsilon)
+        return A
+
+    return policy_fn
+
+
+def q_learning(env,
+               estimator: Estimator,
+               num_episodes: int,
+               discount_factor: float = 1.0,
+               epsilon: float = 0.1,
+               epsilon_decay: float = 1.0) -> EpisodeStats:
+    """
+    Q-Learning algorithm for fff-policy TD control using Function Approximation.
+    Finds the optimal greedy policy while following an epsilon-greedy policy.
+
+    Args:
+        env: OpenAI environment.
+        estimator: Action-Value function estimator
+        num_episodes: Number of episodes to run for.
+        discount_factor: Gamma discount factor.
+        epsilon: Chance the sample a random action. Float betwen 0 and 1.
+        epsilon_decay: Each episode, epsilon is decayed by this factor
+
+    Returns:
+        An EpisodeStats object with two numpy arrays for episode_lengths and episode_rewards.
+    """
+
+    stats = EpisodeStats(length=np.zeros(num_episodes), reward=np.zeros(num_episodes))
+
+    for i in tqdm(range(num_episodes)):
+
+        # The policy we're following
+        policy = make_epsilon_greedy_policy(
+            estimator, epsilon * epsilon_decay ** i, env.action_space.n)
+
+        # Print out which episode we're on, useful for debugging.
+        # Also print reward for last episode
+        last_reward = stats.reward[i - 1]
+
+        # Reset the environment and pick the first action
+        state = env.reset()
+
+        # Only used for SARSA, not Q-Learning
+        next_action = None
+
+        # One step in the environment
+        for t in itertools.count():
+
+            # Choose an action to take
+            # If we're using SARSA we already decided in the previous step
+            if next_action is None:
+                action_probs = policy(state)
+                action = np.random.choice(np.arange(len(action_probs)), p=action_probs)
+            else:
+                action = next_action
+
+            # Take a step
+            next_state, reward, done, _ = env.step(action)
+
+            # Update statistics
+            stats.reward[i] += reward
+            stats.length[i] = t
+
+            # TD Update
+            q_values_next = estimator.predict(next_state)
+
+            # Use this code for Q-Learning
+            # Q-Value TD Target
+            td_target = reward + discount_factor * np.max(q_values_next)
+
+            # Use this code for SARSA TD Target for on policy-training:
+            # next_action_probs = policy(next_state)
+            # next_action = np.random.choice(np.arange(len(next_action_probs)), p=next_action_probs)
+            # td_target = reward + discount_factor * q_values_next[next_action]
+
+            # Update the function approximator using our target
+            estimator.update(state, action, td_target)
+
+            if done:
+                break
+
+            state = next_state
+
+    return stats
+
+
+if __name__ == '__main__':
+    env = gym.envs.make('MountainCar-v0')
+
+    # Feature Preprocessing: Normalize to zero mean and unit variance
+    # We use a few samples from the observation space to do this
+    observation_examples = np.array([env.observation_space.sample() for x in range(10000)])
+    scaler = StandardScaler()
+    scaler.fit(observation_examples)
+
+    # Used to convert a state to a featurizes represenation.
+    # We use RBF kernels with different variances to cover different parts of the space
+    num_components = 100
+    featurizer = FeatureUnion([
+        ('rbf1', RBFSampler(gamma=5.0, n_components=num_components)),
+        ('rbf2', RBFSampler(gamma=2.0, n_components=num_components)),
+        ('rbf3', RBFSampler(gamma=1.0, n_components=num_components)),
+        ('rbf4', RBFSampler(gamma=0.5, n_components=num_components))
+    ])
+    featurizer.fit(scaler.transform(observation_examples))
+
+    estimator = Estimator()
+
+    # Note: For the Mountain Car we don't actually need an epsilon > 0.0
+    # because our initial estimate for all states is too "optimistic" which leads
+    # to the exploration of all states.
+    stats = q_learning(env, estimator, 1000, epsilon=0.0)
+
+    viz.plot_cost_to_go_mountain_car(env, estimator)
+    viz.plot_episode_stats(stats, smoothing_window=25)
--- viz/viz.py
+++ viz/viz.py
@@ -8,6 +8,24 @@ import pandas as pd
 EpisodeStats = namedtuple('Stats', ['length', 'reward'])
 
 
+def plot_cost_to_go_mountain_car(env, estimator, num_tiles: int = 20) -> None:
+    x = np.linspace(env.observation_space.low[0], env.observation_space.high[0], num=num_tiles)
+    y = np.linspace(env.observation_space.low[1], env.observation_space.high[1], num=num_tiles)
+    X, Y = np.meshgrid(x, y)
+    Z = np.apply_along_axis(lambda i: -np.max(estimator.predict(i)), 2, np.dstack([X, Y]))
+
+    fig = plt.figure(figsize=(10, 5))
+    ax = fig.add_subplot(111, projection='3d')
+    surf = ax.plot_surface(X, Y, Z, rstride=1, cstride=1,
+                           cmap=plt.cm.coolwarm, vmin=-1.0, vmax=1.0)
+    ax.set_xlabel('Position')
+    ax.set_ylabel('Velocity')
+    ax.set_zlabel('Value')
+    ax.set_title('Mountain \"Cost To Go\" Function')
+    fig.colorbar(surf)
+    plt.show()
+
+
 def plot_surface(x: np.array, y: np.array, z: np.array, title: str) -> None:
     fig = plt.figure(figsize=(20, 10))
     ax = fig.add_subplot(111, projection='3d')
